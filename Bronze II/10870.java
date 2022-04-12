950b82a76bdb3c005d5bbb4db0e060990fe27704
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-04-12 14:54:49 +0900

    Create 10870.java

---
+++ Bronze II/10870.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args)throws IOException {
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n+1];
+		for(int i=0;i<arr.length;i++)
+		{
+			if(i==0) arr[0]=0;
+			else if(i==1) arr[1]=1;
+			else arr[i]=arr[i-1]+arr[i-2];
+		}
+		System.out.println(arr[n]);
+	}
+}
+
