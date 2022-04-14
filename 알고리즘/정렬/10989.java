8a15c1d6b8815560ab8f5a9de7ec8488dbfe8170
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-04-14 21:01:10 +0900

    Create 10989.java

---
+++ 알고리즘/정렬/10989.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		
+		for(int i=0;i<n;i++)
+		{
+			sb.append(arr[i]).append('\n');
+		}
+		System.out.println(sb);
+	}	
+}
