43b221095a414c7cac036b1adf29cebf2aa473d3
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-04-20 22:42:25 +0900

    Create 1427.java

---
+++ 알고리즘/정렬/1427.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		char[] arr=sc.nextLine().toCharArray();
+		
+		Arrays.sort(arr);
+		
+		for(int i=arr.length-1;i>=0;i--)
+		{
+			System.out.print(arr[i]);
+		}
+		
+	}
+}
