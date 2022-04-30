cccc1f9d9f5c123002b9a6264fd9b87e2eee0351
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-04-30 22:42:31 +0900

    Create 15680.java

---
+++ Bronze IV/15680.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		if(n==0)
+		{
+			System.out.println("YONSEI");
+		}
+		else if(n==1)
+		{
+			System.out.println("Leading the Way to the Future");
+		}
+	}
+}
