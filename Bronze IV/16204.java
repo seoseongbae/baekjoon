3568d0e8d09eaa9f488e06b5630783f8b5f89d9e
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-05-19 21:49:59 +0900

    Create 16204.java

---
+++ Bronze IV/16204.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		 int n = sc.nextInt();
+	        int m = sc.nextInt();
+	        int k = sc.nextInt();
+	        int ans = Math.min(m, k) + Math.min(n-m, n-k);
+	        System.out.println(ans);
+		
+	}
+}
