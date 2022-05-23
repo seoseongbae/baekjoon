6d39b8388f2c94fe2b09cab027703d69167c3d20
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-05-23 21:32:27 +0900

    Create 5337.java

---
+++ bronzeV/5337.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		 System.out.println(".  .   .\n" + "|  | _ | _. _ ._ _  _\n" + "|/\\|(/.|(_.(_)[ | )(/.\n");
+	}
+}
