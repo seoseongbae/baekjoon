8d62dd9595edafa4c03f7c3bf639255f70e702d8
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-05-26 23:15:54 +0900

    Create 16486.java

---
+++ Bronze IV/16486.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int d1 = sc.nextInt(), d2 = sc.nextInt();
+        double pi = 3.141592;
+        System.out.println(d1*2 + 2*pi*d2);
+	}
+}
