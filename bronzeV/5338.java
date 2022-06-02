8862554436222792905c185ee0960279a05094bc
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-06-02 21:51:08 +0900

    Create 5338.java

---
+++ bronzeV/5338.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		  System.out.println("       _.-;;-._");
+	        System.out.println("'-..-'|   ||   |");
+	        System.out.println("'-..-'|_.-;;-._|");
+	        System.out.println("'-..-'|   ||   |");
+	        System.out.print("'-..-'|_.-''-._|");
+	}
+}
