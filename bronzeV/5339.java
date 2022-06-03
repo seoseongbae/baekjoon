61af0be50abe571d411ea124dda9195781292d90
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-06-03 16:36:39 +0900

    Create 5339.java

---
+++ bronzeV/5339.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("     /~\\\n" +
+	            "    ( oo|\n" +
+	            "    _\\=/_\n" +
+	            "   /  _  \\\n" +
+	            "  //|/.\\|\\\\\n" +
+	            " ||  \\ /  ||\n" +
+	            "============\n" +
+	            "|          |\n" +
+	            "|          |\n" +
+	            "|          |");
+	  }
+}
