d2435fa47c996d1cb1987b17a1f08668f9c030c6
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-06-08 22:36:17 +0900

    Create 4101.java

---
+++ bronzeV/4101.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+	
+	
+	public static void main(String[] args) throws IOException{
+		 Scanner sc = new Scanner(System.in);
+
+		    while(true) {
+		      int a = sc.nextInt();
+		      int b = sc.nextInt();
+
+		      if(a == 0 && b == 0) break;
+
+		      System.out.println(a > b ? "Yes" : "No");
+		    }
+	}
+}
