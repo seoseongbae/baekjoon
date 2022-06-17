fd7a1afad4eac2de10ee328aa3ff5e0799e8af4c
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-06-17 23:45:09 +0900

    Create 15962.java

---
+++ bronzeV/15962.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("파이팅!!");
+	}
+}
