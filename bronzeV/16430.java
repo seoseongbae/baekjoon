96b2a290d7f153440cc7618224bc70ea273ba4e2
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-06-19 23:33:33 +0900

    Create 16430.java

---
+++ bronzeV/16430.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println(b-a+" "+b);
+		
+	}
+}
