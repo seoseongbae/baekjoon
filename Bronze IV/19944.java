70683ae1226de7dee6a49e2a328a565c857e9033
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-06-26 23:01:32 +0900

    Create 19944.java

---
+++ Bronze IV/19944.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		 Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt(), m = sc.nextInt();
+	        if (m <= 2)
+	            System.out.println("NEWBIE!");
+	        else if (n < m)
+	            System.out.println("TLE!");
+	        else
+	            System.out.println("OLDBIE!");
+	}
+}
