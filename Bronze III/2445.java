a5aca7eba45ed46b81c73ac1a858ab418b742c5f
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-07-05 21:55:29 +0900

    Create 2445.java

---
+++ Bronze III/2445.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc=new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<=i; j++) {
+				System.out.print('*');
+			}
+			for (int j=1; j<=2*(n-i); j++) {
+				System.out.print(" ");
+			}
+			for (int j=1; j<=i; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+		
+		for (int i=n-1; i>=1; i--) {
+			for (int j=1; j<=i; j++) {
+				System.out.print('*');
+			}
+			for (int j=1; j<=2*(n-i); j++) {
+				System.out.print(" ");
+			}
+			for (int j=1; j<=i; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+}
