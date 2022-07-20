412339c508f888fa85b38f77666b0168e507e38a
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-07-20 22:59:36 +0900

    Create 2566.java

---
+++ Bronze III/2566.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[][] arr = new int[9][9];
+
+		for (int i = 0; i < 9; i++) {
+			String[] strArr = br.readLine().split(" ");
+			for (int j = 0; j < strArr.length; j++) {
+				arr[i][j] = Integer.parseInt(strArr[j]);
+			}
+		}
+
+		int num = 0;
+		int rowOrder = 0;
+		int columnOrder = 0;
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (num < arr[i][j]) {
+					num = arr[i][j];
+					rowOrder = i;
+					columnOrder = j;
+				}
+			}
+		}
+
+		System.out.println(num);
+		System.out.println((rowOrder + 1) + " " + (columnOrder + 1));
+}
+}
