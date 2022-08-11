c6a924ada64cdef458e017a666a2d9565f742d9e
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-08-11 22:55:38 +0900

    Create 1100.java

---
+++ Bronze II/1100.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        int count = 0;
+	        String[][] map = new String[8][8];
+	        
+	        for(int i=0; i<map.length; i++) {
+	            String[] str = br.readLine().split("");
+	            for(int j=0; j<map[0].length; j++) {
+	                map[i][j] = str[j];
+	            }
+	        }
+	        
+	        for(int i=0; i<map.length; i++) {
+	            for(int j=0; j<map[0].length; j++) {
+	                if((i+j)%2 == 0 && map[i][j].equals("F")) {
+	                    count++;
+	                }
+	            }
+	        }
+	        
+	        System.out.println(count);
+	}
+}
