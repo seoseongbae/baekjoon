82d90dd25f53a5651ad7b0f2a59c0c7784601651
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-08-30 22:33:42 +0900

    Create 2720.java

---
+++ 수학/2720.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+ 
+        while(T!=0){
+            int cost = sc.nextInt();
+ 
+            int a = (int)cost/25;
+            int b = (int)(cost-a*25)/10;
+            int c = (int)(cost-a*25-b*10)/5;
+            int d = cost-a*25-b*10-c*5;
+            System.out.println(a+ " "+b+ " "+c+ " "+d);                                                  
+            T--;
+        }
+	}
+}
