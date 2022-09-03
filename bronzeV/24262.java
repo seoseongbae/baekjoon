781d6dada140dae9760d7f79e33c99ba1a83dd18
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-09-03 22:47:18 +0900

    Create 24262.java

---
+++ bronzeV/24262.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc=new Scanner(System.in);
+		System.out.println(1);
+		System.out.println(0);
+	}
+}
